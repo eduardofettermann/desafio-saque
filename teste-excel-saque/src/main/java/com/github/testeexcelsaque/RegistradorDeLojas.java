51ddd7b943e08c34962017bf45887cb569208861
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-08 03:16:00 -0300

    feat(ResgistradorDeLojas): adiciona base da solucao, conseguindo converteros dados da tabela em HashMap

--- teste-excel-saque/src/main/java/com/github/testeexcelsaque/RegistradorDeLojas.java
+++ teste-excel-saque/src/main/java/com/github/testeexcelsaque/RegistradorDeLojas.java
@@ -12,35 +12,40 @@ public class RegistradorDeLojas {
 
         HashMap<String, LojaDTO> lojas = new HashMap<>();
         String[] dadosDaLinha = {"mockado", "mock", "mockei"};
-        while (dadosDaLinha.length < 2) {
-            dadosDaLinha = leitor.retornaProximaLinhaFracionada();
-            for (String dadoDaLinha : dadosDaLinha) {
-                System.out.println(dadoDaLinha);
+        try {
+
+            while (!dadosDaLinha[0].equals("Array")) {
+                dadosDaLinha = leitor.retornaProximaLinhaFracionada();
+                for (String dadoDaLinha : dadosDaLinha) {
+                    System.out.println(dadoDaLinha);
+                }
+                System.out.println("codigo da loja " + dadosDaLinha[0]);
+                System.out.println("tamanho da array: " + dadosDaLinha.length);
+                String codigoDaLoja = dadosDaLinha[0];
+                System.out.println("codigo da loja atribuido: " + codigoDaLoja);
+                String funcaoDRE = dadosDaLinha[1];
+                System.out.println("funcao atribuida: " + funcaoDRE);
+                double resultadoDRE = Double.parseDouble(dadosDaLinha[2].replace(",", "."));
+                System.out.println("resultado atribuido " + resultadoDRE);
+                LojaDTO lojaDTO = new LojaDTO();
+                if (!lojas.containsKey(codigoDaLoja)) {
+                    lojaDTO.setCodigoDaLoja(codigoDaLoja);
+                    lojas.put(codigoDaLoja, lojaDTO);
+                }
+                switch (funcaoDRE) {
+                    case ("Receitas") -> lojas.get(codigoDaLoja).setReceitas(resultadoDRE);
+                    case ("Impostos") -> lojas.get(codigoDaLoja).setImpostos(resultadoDRE);
+                    case ("Custo") -> lojas.get(codigoDaLoja).setCusto(resultadoDRE);
+                    case ("Despesa") -> lojas.get(codigoDaLoja).setDespesa(resultadoDRE);
+                    case ("Resultado Financeiro") -> lojas.get(codigoDaLoja).setResultadoFinanceiro(resultadoDRE);
+                    case ("Provisão/Perda") -> lojas.get(codigoDaLoja).setProvisaoBarraPerda(resultadoDRE);
+                    case ("PPR") -> lojas.get(codigoDaLoja).setPpr(resultadoDRE);
+                    case ("IR/CSLL") -> lojas.get(codigoDaLoja).setIrBarraCsll(resultadoDRE);
+                }
+                System.out.println("Informacao atribuida");
             }
-            System.out.println("codigo da loja " + dadosDaLinha[0]);
-            System.out.println("tamanho da array: " + dadosDaLinha.length);
-            String codigoDaLoja = dadosDaLinha[0];
-            System.out.println("codigo da loja atribuido: " + codigoDaLoja);
-            String funcaoDRE = dadosDaLinha[1];
-            System.out.println("funcao atribuida: " + funcaoDRE);
-            double resultadoDRE = Double.parseDouble(dadosDaLinha[2].replace(",","."));
-            System.out.println("resultado atribuido " + resultadoDRE);
-            LojaDTO lojaDTO = new LojaDTO();
-            if (!lojas.containsKey(codigoDaLoja)) {
-                lojaDTO.setCodigoDaLoja(codigoDaLoja);
-                lojas.put(codigoDaLoja, lojaDTO);
-            }
-            switch (funcaoDRE) {
-                case ("Receitas") -> lojas.get(codigoDaLoja).setReceitas(resultadoDRE);
-                case ("Impostos") -> lojas.get(codigoDaLoja).setImpostos(resultadoDRE);
-                case ("Custo") -> lojas.get(codigoDaLoja).setCusto(resultadoDRE);
-                case ("Despesa") -> lojas.get(codigoDaLoja).setDespesa(resultadoDRE);
-                case ("Resultado Financeiro") -> lojas.get(codigoDaLoja).setResultadoFinanceiro(resultadoDRE);
-                case ("Provisão/Perda") -> lojas.get(codigoDaLoja).setProvisaoBarraPerda(resultadoDRE);
-                case ("PPR") -> lojas.get(codigoDaLoja).setPpr(resultadoDRE);
-                case ("IR/CSLL") -> lojas.get(codigoDaLoja).setIrBarraCsll(resultadoDRE);
-            }
-            System.out.println("Informacao atribuida");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Ocorreu um erro: Provavelmente foi encontrada uma linha com ';' separadores mas sem informações nas cédulas ");
         }
         leitor.fechaLeitor();
         return lojas;
