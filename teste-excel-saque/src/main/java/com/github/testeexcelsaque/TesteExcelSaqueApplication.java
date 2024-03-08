98da8ef2c9e6a8da288a960cd9a15e325f4a3986
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-08 11:24:38 -0300

    style(TesteExcelSaqueAplication): troca exibicao da main()

--- teste-excel-saque/src/main/java/com/github/testeexcelsaque/TesteExcelSaqueApplication.java
+++ teste-excel-saque/src/main/java/com/github/testeexcelsaque/TesteExcelSaqueApplication.java
@@ -1,11 +1,11 @@
 package com.github.testeexcelsaque;
 
-import com.github.testeexcelsaque.view.Menu;
+import com.github.testeexcelsaque.service.LojaService;
 
 public class TesteExcelSaqueApplication {
 
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.exibeMenuPrincipal();
+        LojaService service = new LojaService();
+        service.exibeTodasLojas();
     }
 }
