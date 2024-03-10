32cff3ddfcc91a27417df954c83360b64fa57d97
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-10 02:16:13 -0300

    feat(ConverteOpostoDoNumero): adiciona classe com metodo de converter numero para seu oposto

---
+++ teste-excel-saque/src/main/java/com/github/testeexcelsaque/util/ConverteOpostoDoNumero.java
@@ -0,0 +1,7 @@
+package com.github.testeexcelsaque.util;
+
+public class ConverteOpostoDoNumero {
+    public static double retornaOpostoDoNumero(double valor) {
+        return valor * -1;
+    }
+}
