ae5c2417ef482a9f67e296cb233ef459585d3d77
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-10 02:13:37 -0300

    feat(lojaService): adiciona metodo retornaTodasLojasComDetalhe e retornaLojasDTOList

--- teste-excel-saque/src/main/java/com/github/testeexcelsaque/service/LojaService.java
+++ teste-excel-saque/src/main/java/com/github/testeexcelsaque/service/LojaService.java
@@ -1,21 +1,32 @@
 package com.github.testeexcelsaque.service;
 
 import com.github.testeexcelsaque.loja.LojaDTO;
+import com.github.testeexcelsaque.loja.LojaModel;
 import com.github.testeexcelsaque.util.ConversorDeCsvParaHashMap;
 import com.github.testeexcelsaque.util.ConversorDeHashMapParaList;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
 public class LojaService {
-    private ConversorDeCsvParaHashMap conversorDeCsvParaHashMap = new ConversorDeCsvParaHashMap();
-    private ConversorDeHashMapParaList conversorDeHashMapParaList = new ConversorDeHashMapParaList();
+    private final ConversorDeCsvParaHashMap conversorDeCsvParaHashMap = new ConversorDeCsvParaHashMap();
+    private final ConversorDeHashMapParaList conversorDeHashMapParaList = new ConversorDeHashMapParaList();
 
-    public void exibeTodasLojas() {
-        HashMap<String, LojaDTO> lojasHashMap = conversorDeCsvParaHashMap.retornaHashMapDeLojas();
-        List<LojaDTO> lojasList = conversorDeHashMapParaList.retornaLojasConvertidasEmList(lojasHashMap);
-        lojasList.forEach(lojaDTO -> {
-            System.out.println(lojaDTO.toString());
+    public HashMap<String, LojaDTO> retornaLojasDTOHashMap() {
+        return conversorDeCsvParaHashMap.retornaHashMapDeLojas();
+    }
+
+    public List<LojaDTO> retornaLojasDTOList() {
+        return conversorDeHashMapParaList.retornaLojasConvertidasEmList(this.retornaLojasDTOHashMap());
+    }
+
+    public List<LojaModel> retornaTodasLojasComDetalhes() {
+        List<LojaDTO> lojasDTO = this.retornaLojasDTOList();
+        List<LojaModel> lojasComDetalhes = new ArrayList<>();
+        lojasDTO.forEach(lojaDTO -> {
+            lojasComDetalhes.add(new LojaModel(lojaDTO));
         });
     }
 
