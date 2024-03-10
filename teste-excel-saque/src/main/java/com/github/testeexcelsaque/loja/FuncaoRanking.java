685ece620e01eb96350bc22c18d14b4a2b4664cd
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-10 02:14:41 -0300

    feat(FuncaoRanking): adiciona enum de funcoes de ordenacao

---
+++ teste-excel-saque/src/main/java/com/github/testeexcelsaque/loja/FuncaoRanking.java
@@ -0,0 +1,19 @@
+package com.github.testeexcelsaque.loja;
+
+public enum FuncaoRanking {
+    RECEITA_LIQUIDA("receitaLiquida"),
+    CUSTO("custo"),
+    DESPESA("despesa"),
+    RESULTADO_OPERACIONAL("resultadoOperacional"),
+    MARGEM_OPERACIONAL("margemOperacional"),
+    LUCRATIVIDADE("lucratividade");
+    private String valor;
+
+    FuncaoRanking(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return this.valor;
+    }
+}
